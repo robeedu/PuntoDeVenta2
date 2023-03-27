187073a064b005e6b86218102f61a757f8b7f7f5
robeedu <126532961+robeedu@users.noreply.github.com>
2023-03-27 14:40:41 -0600

    Holarobe

--- PuntoDeVenta2-ejb/src/java/mx/sistemas/modelo/Categoria.java
+++ PuntoDeVenta2-ejb/src/java/mx/sistemas/modelo/Categoria.java
@@ -22,7 +22,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
-
+///Rober 
 /**
  *
  * @author Rober
